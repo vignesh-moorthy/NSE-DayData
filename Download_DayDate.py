7eca339405f5c4eee84d9ffbb721cedfb04157b2
vignesh <vigneshwaramoorthys@gmail.com>
2019-10-16 01:15:45 +0530

    Imporved UX by improving logs.

--- Download_DayDate.py
+++ Download_DayDate.py
@@ -13,9 +13,10 @@ Start_date="09OCT2019"
 End_date="12OCT2019"
 """
 
-import requests, zipfile, io
+import requests, zipfile, io,logging
 import pandas as pd
 from datetime import datetime,timedelta
+
 
 Start_date="enter_start_date_in_DDMMMYYYY"
 End_date="enter_end_date_in_DDMMMYYYY"
@@ -24,13 +25,15 @@ End_date="enter_end_date_in_DDMMMYYYY"
 #End_date="12OCT2019"
 #If no date is given, then it will download the files from the 7 previous days
 
-d_path='Path_to_download_the_csv_file'
+d_path="Path_to_download_the_csv_file"
 #Example: d_path='D:\Trade\Day_Bhavcopy.'
 #If you fail to specify a path, the csv files will get downloaded in the path where this code is saved(Under /Path_to_download_the_extracted_file).
 
 #To print Number of working days/files downloaded
-global No_of_download
+global No_of_download,Working_day,Non_Work_day
 No_of_download=0
+Working_day=0
+Non_Work_day=0
 
 #Def for downloading and Unzipping the File    
 def req(zip_file_url):
@@ -41,24 +44,28 @@ def req(zip_file_url):
     #If status code is <> 200, it indicates that no data is present for that date. For example, week-end, or trading holiday.
     if status_code==200:
         No_of_download=No_of_download+1
-        print("File Available.Downloading")
+        logger.info("File Available.Downloading")
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall(path=d_path)
     else:
-        print("File Not Available.Moving to next date.")
+        logger.info("******File Not Available.Moving to next date.")
+
 
 
 #Main Program
-today_date=datetime.now()
+today_date=datetime.now().strftime("%Y%b%d")
+logging.basicConfig(filename="Log_"+today_date+".log", format='%(asctime)s %(message)s', filemode='w') 
+logger=logging.getLogger() 
+logger.setLevel(logging.INFO) 
 
 #Populating today's date as default, if the stat_date and/or End_date is not provided.
 if Start_date=="" or Start_date=="enter_start_date_in_DDMMMYYYY":
-    Start_date=(datetime.now()-timedelta(days=7))
+    Start_date=(datetime.now()-timedelta(days=7)).strftime("%Y%b%d")
     End_date=today_date
 if End_date=="" or End_date=="enter_start_date_in_DDMMMYYYY":
         End_date=today_date
 
-daterange = pd.date_range(Start_date,End_date)
+daterange = pd.date_range(datetime.strptime(Start_date, "%Y%b%d"),datetime.strptime(End_date, "%Y%b%d"))
 
 #Looping through each date, and downloading the file.
 for single_date in daterange:
@@ -68,21 +75,18 @@ for single_date in daterange:
     weekday=single_date.weekday()
     #If day is not Saturday or Sunday,then proceed to download the file.
     if weekday not in [5,6]:
-        print(loop_date)
+        Working_day=Working_day+1
+        logger.info("Trying to download File of :"+loop_date)
         temp_zip_file_url = 'https://www.nseindia.com/content/historical/EQUITIES/'+year+'/'+month+'/cm'+date+month+year+'bhav.csv.zip'
         req(zip_file_url=temp_zip_file_url)
         #print(temp_zip_file_url)
-
-print("Number of files downloaded:"+str(No_of_download))
-
-"""
-count=0
-for single_date in daterange:
-    loop_date=single_date.strftime("%Y-%b-%d")
-    year,month,date=loop_date.split('-')
-    month=month.upper()
-    weekday=single_date.weekday()
-    if weekday not in [5,6]:
-        count=count+1
-print(count)
-"""
+    else:
+        Non_Work_day=Non_Work_day+1
+        
+#print("Number of files downloaded:"+str(No_of_download))
+logger.info("****************************************************************************************") 
+logger.info("No. of files downloaded="+str(No_of_download)) 
+logger.info("Span= " + Start_date+ " to " + End_date )
+logger.info("No. of weekdays in the given time span="+str(Working_day)) 
+logger.info("****************************************************************************************") 
+logging.shutdown()
